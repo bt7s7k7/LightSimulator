56d16924f28301ac186440fa8e002f3eae8c795a
bt7s7k7 <bt7s7k7@hotmail.sk>
2020-01-16 20:46:52 +0100

    Event loop

--- LightSimulator/update.cpp
+++ LightSimulator/update.cpp
@@ -3,5 +3,17 @@
 
 void update() {
 	spdlog::info("Event loop starting");
+
+	while (true) {
+		SDL_Event event;
+		while (SDL_PollEvent(&event)) {
+			if (event.type == SDL_WINDOWEVENT) {
+				if (event.window.event == SDL_WINDOWEVENT_CLOSE) {
+					goto eventLoopExit;
+				}
+			}
+		}
+	}
+	eventLoopExit:
 	spdlog::info("Event loop ended");
 }
